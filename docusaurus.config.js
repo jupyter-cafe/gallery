7fbf315a61fecaad620996cf8e3effe78800dd5d
prionkor <sisir@codeware.io>
2022-08-14 15:18:06 +0600

    config update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,8 +80,9 @@ const config = {
 					// { to: "/blog", label: "Blog", position: "left" },
 					{
 						href: 'https://github.com/jupyter-cafe/menu',
-						label: 'GitHub',
 						position: 'right',
+						className: 'header-github-link',
+						'aria-label': 'GitHub repository',
 					},
 				],
 			},
